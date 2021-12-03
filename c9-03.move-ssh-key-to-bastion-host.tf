844b1de91b7d704ba8dfbce8304d0fa7875dc66d
gopal <gopal1409@gmail.com>
2021-12-03 17:49:18 +0530

    azurerm with bastion service

--- c9-03.move-ssh-key-to-bastion-host.tf
+++ c9-03.move-ssh-key-to-bastion-host.tf
@@ -7,17 +7,17 @@ resource "null_resource" "null_copy_ssh_key_to_bastion" {
     type = "ssh"
     host = azurerm_linux_virtual_machine.bastion_host_linuxvm.public_ip_address
     user = azurerm_linux_virtual_machine.bastion_host_linuxvm.admin_username
-    private_key = file("${path.module}/ssh-keys/terraform-azzure.pub")
+    private_key = file("${path.module}/ssh-keys/terraform-azzure.pem")
   }
   #this fil;e provisoner will copy one file inside your vm
   provisioner "file" {
-    source = "ssh-keys/terraform-azzure.pub"
-    destination = "/tmp/terraform-azzure.pub"
+    source = "ssh-keys/terraform-azzure.pem"
+    destination = "/tmp/terraform-azzure.pem"
   }
   #inside your vm if you want to run some command while executing terraform apply
   provisioner "remote-exec" {
     inline = [
-      "sudo chmod 400 /tmp/terraform-azzure.pub"
+      "sudo chmod 400 /tmp/terraform-azzure.pem"
       #"ansible-playbook nameofplay.yaml"
       
     ]
