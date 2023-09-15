955f1cbed0268da7c07b03ca00d6cd1b7c6d9c85
gopal <gopal1409@gmail.com>
2023-09-15 09:53:08 +0530

    test

--- c8-05.web-linuxvm-resource.tf
+++ c8-05.web-linuxvm-resource.tf
@@ -11,9 +11,9 @@ sudo systemctl disable firewalld
 sudo chmod -R 777 /var/www/html 
 sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
 sudo mkdir /var/www/html/app1
-sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
-sudo echo "Welcome to stacksimplify - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
-sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - WebVM APP-1 </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+sudo echo "Welcome to lifesimple - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
+sudo echo "Welcome to lifesimple - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
+sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to lifesimple - WebVM APP-1 </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
 sudo curl -H "Metadata:true" --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-09-01" -o /var/www/html/app1/metadata.html
 CUSTOM_DATA  
 }
@@ -38,12 +38,12 @@ resource "azurerm_linux_virtual_machine" "web_linuxvm" {
     storage_account_type = "Standard_LRS"
   }
   source_image_reference {
-    publisher = "RedHat"
-    offer = "RHEL"
-    sku = "83-gen2"
-    version = "latest"
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
   }
-  #custom_data = filebase64("${path.module}/app-scripts/redhat-webvm-script.sh")    
-  custom_data = base64encode(local.webvm_custom_data)  
+  custom_data = filebase64("${path.module}/app-script/app-script.sh")    
+  #custom_data = base64encode(local.webvm_custom_data)  
 
 }
